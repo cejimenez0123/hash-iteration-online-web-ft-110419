e259f09b86ea0afe4d1c8680e939ebcdb8d1b80f
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-12 18:26:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name,age|
     if age > 12
       return "You're too old for this"
-    else happy_birthday(kids_name)
+    else "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
   end
